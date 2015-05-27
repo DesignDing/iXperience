4c83959bb1698a42d8f07aff2f2299ca22707eea
DesignDing <sd4ue@virginia.edu>
2015-05-27 21:02:35 +0200

    Finish

---
+++ apps/lekker_plekke/db/seeds.rb
@@ -0,0 +1,16 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
+Place.delete_all
+
+Place.create(name: 'Kirstenbosch Botanical Gardens', description: 'stunning garden, has great treetop canopy walkway', 
+	location: 'Rhodes Dr
+Newlands Cape Town 7735')
+Place.create(name:  'Clifton 4th beach', description: 'gorgeous stretch of sand and sea', 
+location: 'Clifton Cape Town 8005')
+Place.create(name: 'Rhodes Memoria', description:'a chilled spot for selfies and relaxing with friends', 
+location: 'Rhodes Avenue Cape Town')
